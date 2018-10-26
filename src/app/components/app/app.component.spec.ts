e983ee1be60b3e2b49d527e5834f7b023fc6c23d
Dan Rullo <drullo@cleavelandprice.com>
2018-10-26 15:32:46 -0400

    Trying to fix assets issue on GH pages

--- src/app/components/app/app.component.spec.ts
+++ src/app/components/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'wmtr-quote'`, () => {
+  it(`should have as title 'wmtrquote'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('wmtr-quote');
+    expect(app.title).toEqual('wmtrquote');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to wmtr-quote!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to wmtrquote!');
   });
 });
